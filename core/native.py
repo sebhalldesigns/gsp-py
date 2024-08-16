f0d60b76a13b021c630014f9ccbbddc69d110ea1
Seb Hall <sebhalldesigns@gmail.com>
2024-08-16 10:31:14 +0100

    working windowing stuff

--- core/native.py
+++ core/native.py
@@ -18,3 +18,44 @@ class gsp_list:
         f_gsp_list_destroy_list.argtypes = [ctypes.c_void_p]
         f_gsp_list_destroy_list(list)
 
+class gwindow_event_t(ctypes.Structure):
+    _fields_ = [("event_id", ctypes.c_uint8),
+                ("sub_id", ctypes.c_uint8),
+                ("data", ctypes.c_uint64)]
+
+class gsp_window:
+    
+    lib = CDLL(util.find_library("GSPCore.dll"))
+    EVENT_CALLBACK_TYPE = ctypes.CFUNCTYPE(None, ctypes.c_void_p, gwindow_event_t)
+
+    @staticmethod
+    def gsp_window_poll_events():
+        f_gsp_window_poll_events = gsp_list.lib.gsp_window_poll_events
+        return f_gsp_window_poll_events()
+
+    @staticmethod
+    def gsp_window_is_window_valid(window):
+        f_gsp_window_is_window_valid = gsp_list.lib.gsp_window_is_window_valid
+        f_gsp_window_is_window_valid.argtypes = [ctypes.c_void_p]
+        f_gsp_window_is_window_valid.restype = ctypes.c_bool
+        return f_gsp_window_is_window_valid()
+    
+    @staticmethod
+    def gsp_window_create_window():
+        f_gsp_window_create_window = gsp_list.lib.gsp_window_create_window
+        f_gsp_window_create_window.restype = ctypes.c_void_p
+        return f_gsp_window_create_window()
+
+    @staticmethod
+    def gsp_window_set_title(window, title):
+        f_gsp_window_set_title = gsp_list.lib.gsp_window_set_title
+        f_gsp_window_set_title.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
+        return f_gsp_window_set_title(window, title.encode("utf-8"))
+    
+    @staticmethod
+    def gsp_window_set_event_callback(window, callback_handle):
+        f_gsp_window_set_event_callback = gsp_list.lib.gsp_window_set_event_callback
+        f_gsp_window_set_event_callback.argtypes = [ctypes.c_void_p, gsp_window.EVENT_CALLBACK_TYPE]
+        return f_gsp_window_set_event_callback(window, callback_handle)
+
+    
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from core.native import gsp_list
+from core.native import gsp_list, gsp_window
+import sys
 
-list1 = gsp_list.gsp_list_create_list()
-list2 = gsp_list.gsp_list_create_list()
-list3 = gsp_list.gsp_list_create_list()
+window1 = gsp_window.gsp_window_create_window()
+should_quit = False
 
-gsp_list.gsp_list_destroy_list(list1)
-gsp_list.gsp_list_destroy_list(list2)
-gsp_list.gsp_list_destroy_list(list3)
+def callback(window, event):
+    print(f"python callback {event.event_id}")
+    if event.event_id == 7:
+        should_quit = True
+
+callback_handle = gsp_window.EVENT_CALLBACK_TYPE(callback)
+
+gsp_window.gsp_window_set_title(window1, "hello from python")
+gsp_window.gsp_window_set_event_callback(window1, callback_handle)
+
+while should_quit == False:
+    print(f"should quit {should_quit}")
+    gsp_window.gsp_window_poll_events()
