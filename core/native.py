40915dc239ab1634905406f788570a96572915cd
Seb Hall <sebhalldesigns@gmail.com>
2024-08-18 19:24:00 +0100

    updates

--- core/native.py
+++ core/native.py
@@ -2,6 +2,7 @@ from ctypes import util, CDLL
 import ctypes
 import os
 
+
 class gsp_list:
     
     lib = CDLL(util.find_library("GSPCore.dll"))
@@ -17,6 +18,36 @@ class gsp_list:
         f_gsp_list_destroy_list = gsp_list.lib.gsp_list_destroy_list
         f_gsp_list_destroy_list.argtypes = [ctypes.c_void_p]
         f_gsp_list_destroy_list(list)
+
+class gsp_tree:
+    
+    lib = CDLL(util.find_library("GSPCore.dll"))
+
+    @staticmethod
+    def gsp_tree_create_root_node():
+        f_gsp_tree_create_root_node = gsp_list.lib.gsp_tree_create_root_node
+        f_gsp_tree_create_root_node.restype = ctypes.c_void_p
+        return f_gsp_tree_create_root_node()
+
+    @staticmethod
+    def gsp_tree_create_child_node(parent):
+        f_gsp_tree_create_child_node = gsp_list.lib.gsp_tree_create_child_node
+        f_gsp_tree_create_child_node.argtypes = [ctypes.c_void_p]
+        f_gsp_tree_create_child_node.restype = ctypes.c_void_p
+        return f_gsp_tree_create_child_node(parent)
+
+    @staticmethod
+    def gsp_tree_destroy_node(node):
+        f_gsp_tree_destroy_node = gsp_list.lib.gsp_tree_destroy_node
+        f_gsp_tree_destroy_node.argtypes = [ctypes.c_void_p]
+        f_gsp_tree_destroy_node(node)
+
+    @staticmethod
+    def gsp_tree_check_node_exists(node):
+        f_gsp_tree_check_node_exists = gsp_list.lib.gsp_tree_check_node_exists
+        f_gsp_tree_check_node_exists.argtypes = [ctypes.c_void_p]
+        f_gsp_tree_check_node_exists.restype = ctypes.c_bool
+        return f_gsp_tree_check_node_exists(node)
 
 class gwindow_event_t(ctypes.Structure):
     _fields_ = [("event_id", ctypes.c_uint8),
@@ -57,5 +88,55 @@ class gsp_window:
         f_gsp_window_set_event_callback = gsp_list.lib.gsp_window_set_event_callback
         f_gsp_window_set_event_callback.argtypes = [ctypes.c_void_p, gsp_window.EVENT_CALLBACK_TYPE]
         return f_gsp_window_set_event_callback(window, callback_handle)
+
+class gcolor_t(ctypes.Structure):
+    _fields_ = [("red", ctypes.c_float),
+                ("green", ctypes.c_float),
+                ("blue", ctypes.c_float),
+                ("alpha", ctypes.c_float)]
+
+class gsp_view:
+
+    lib = CDLL(util.find_library("GSPCore.dll"))
+
+    @staticmethod
+    def gsp_view_create_view():
+        f_gsp_view_create_view = gsp_view.lib.gsp_view_create_view
+        f_gsp_view_create_view.restype = ctypes.c_void_p
+        return f_gsp_view_create_view()
+
+    @staticmethod
+    def gsp_view_is_view_valid(view):
+        f_gsp_view_is_view_valid = gsp_view.lib.gsp_view_is_view_valid
+        f_gsp_view_is_view_valid.argtypes = [ctypes.c_void_p]
+        f_gsp_view_is_view_valid.restype = ctypes.c_bool
+        return f_gsp_view_is_view_valid(view)
+    
+    @staticmethod
+    def gsp_view_is_view_root(view):
+        f_gsp_view_is_view_root = gsp_view.lib.gsp_view_is_view_root
+        f_gsp_view_is_view_root.argtypes = [ctypes.c_void_p]
+        f_gsp_view_is_view_root.restype = ctypes.c_bool
+        return f_gsp_view_is_view_root(view)
+    
+    @staticmethod
+    def gsp_view_set_window_root(window, view):
+        f_gsp_view_set_window_root = gsp_view.lib.gsp_view_set_window_root
+        f_gsp_view_set_window_root.argtypes = [ctypes.c_void_p, ctypes.c_void_p]
+        f_gsp_view_set_window_root(window, view)
+
+    @staticmethod
+    def gsp_view_add_child(parent, child):
+        f_gsp_view_add_child = gsp_view.lib.gsp_view_add_child
+        f_gsp_view_add_child.argtypes = [ctypes.c_void_p, ctypes.c_void_p]
+        f_gsp_view_add_child.restype = ctypes.c_bool
+        return f_gsp_view_add_child(parent, child)
+        
+
+    @staticmethod
+    def gsp_view_set_background_color(view, color):
+        f_gsp_view_set_background_color = gsp_view.lib.gsp_view_set_background_color
+        f_gsp_view_set_background_color.argtypes = [ctypes.c_void_p, gcolor_t]
+        f_gsp_view_set_background_color(view, color)
 
     
--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-from core.native import gsp_list, gsp_window
+from core.native import gsp_list, gsp_window, gsp_view, gsp_tree, gcolor_t
 import sys
 
 window1 = gsp_window.gsp_window_create_window()
 should_quit = False
+
+#root1 = gsp_tree.gsp_tree_create_root_node()
+#root2 = gsp_tree.gsp_tree_create_root_node()
+#node1 = gsp_tree.gsp_tree_create_child_node(root1)
+#node2 = gsp_tree.gsp_tree_create_child_node(node1)
+#node3 = gsp_tree.gsp_tree_create_child_node(root1)
+#node4 = gsp_tree.gsp_tree_create_child_node(root2)
+
+#gsp_tree.gsp_tree_destroy_node(root1)
+#
+
+view1 = gsp_view.gsp_view_create_view()
+view2 = gsp_view.gsp_view_create_view()
+
+gsp_view.gsp_view_add_child(view1, view2)
+
+background = gcolor_t()
+background.red = 1.0
+background.green = 1.0
+background.blue = 1.0
+background.alpha = 1.0
+
+gsp_view.gsp_view_set_background_color(view1, background)
+gsp_view.gsp_view_set_window_root(window1, view1)
+
 
 def callback(window, event):
     print(f"python callback {event.event_id}")
@@ -13,6 +38,8 @@ callback_handle = gsp_window.EVENT_CALLBACK_TYPE(callback)
 
 gsp_window.gsp_window_set_title(window1, "hello from python")
 gsp_window.gsp_window_set_event_callback(window1, callback_handle)
+
+view1 = gsp_view.gsp_view_create_view()
 
 while should_quit == False:
     print(f"should quit {should_quit}")
